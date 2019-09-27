9e74836f30e9abe11dcf30d5916c234eaf9c808a
saltytt <darkfirekiller125@hotmail.com>
2019-09-27 12:58:14 -0400

    1.3 (57) and other fixes

--- assets/js/multicontroller.js
+++ assets/js/multicontroller.js
@@ -1,6 +1,6 @@
 function updateDL() {
     $.get("https://api.github.com/repos/saltyTT/MultiController/releases", function(data, status) {
         for (var i = 0; i < data.length; i++)
-            $("#dlc" + data[i].tag_name.replace(".","_")).html(data[i].assets[0].download_count + " downloads");
+            $("#dlc" + data[i].tag_name.replace(/\./g ,"_")).html(data[i].assets[0].download_count + " downloads");
     })
 }
